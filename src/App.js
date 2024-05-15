2bca48c579eb9ccf1177a1d533d0ef64738562a6
revivesanjeev <revivesanjeev324@gmail.com>
2024-05-15 18:01:48 +0530

    passing data from parent to child using props

--- src/App.js
+++ src/App.js
@@ -4,9 +4,31 @@ function App() {
   return (
     <div>
       <h1>Let's get Started</h1>
-<ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        location="Bangalore"
+        date={new Date(2023, 7, 15)}
+        title="Insurance"
+        price="50"
+      ></ExpenseItem>
+      <ExpenseItem
+        location="Delhi"
+        date={new Date(2023, 3, 25)}
+        title="Book"
+        price="20"
+      ></ExpenseItem>
+      <ExpenseItem
+        location="Hyderabad"
+        date={new Date(2023, 10, 11)}
+        title="Pen"
+        price="5"
+      ></ExpenseItem>
+      <ExpenseItem
+        location="Mumbai"
+        date={new Date(2023, 1, 14)}
+        title="Laptop"
+        price="200"
+      ></ExpenseItem>
     </div>
   );
 }
-
 export default App;
